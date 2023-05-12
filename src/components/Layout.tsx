e2343900f05f4a1b6000e374dc700f39918caf75
naufalfakhar <direct.anaufal@gmail.com>
2023-05-12 18:23:19 +0700

    fix: remove unused import

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,3 @@
-import { TailwindIndicator } from './TailwindIndicator'
 import { cn } from '@/lib/utils'
 import Navbar from '@/components/Navbar'
 import { Outlet, useLocation } from 'react-router-dom'
